6a79470123ef7bce784347255f35f8cb372dc5b5
C-Iahny <miarisoa00@yahoo.de>
2026-07-27 19:18:22 +0200

    fix: ordre URLs tourisme (proposer avant slug) + migrations manquantes

--- tourisme/urls.py
+++ tourisme/urls.py
@@ -6,9 +6,9 @@ app_name = 'tourisme'
 urlpatterns = [
     path('',                     views.tourisme_home,   name='home'),
     path('lieux/',               views.lieux_list,      name='lieux_list'),
+    path('lieux/proposer/',      views.lieu_submit,     name='lieu_submit'),
     path('lieux/<slug:slug>/',   views.lieu_detail,     name='lieu_detail'),
     path('guides/',              views.guides_list,     name='guides_list'),
-    path('guides/<int:pk>/',     views.guide_profile,   name='guide_profile'),
     path('guides/inscription/',  views.guide_register,  name='guide_register'),
-    path('lieux/proposer/',      views.lieu_submit,     name='lieu_submit'),
+    path('guides/<int:pk>/',     views.guide_profile,   name='guide_profile'),
 ]
